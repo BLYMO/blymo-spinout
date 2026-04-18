e3c0d2a2e1dfed3b836258b8a5bfa476b1a7c01a
bastiyan <bastiyan@blymo.co..uk>
2026-04-18 03:26:49 +0100

    codebuild

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -166,6 +166,11 @@ resource "aws_iam_role_policy" "codebuild_terraform_permissions" {
           "rds:DescribeDBSubnetGroups",
           "rds:ListTagsForResource",
           "rds:ModifyDBInstance",
+          "rds:DescribeDBParameterGroups",
+          "rds:CreateDBParameterGroup",
+          "rds:ModifyDBParameterGroup",
+          "rds:DeleteDBParameterGroup",
+          "rds:AddTagsToResource",
           # Secrets Manager (full access — Terraform may create/delete secrets during state reconciliation)
           "secretsmanager:GetSecretValue",
           "secretsmanager:DescribeSecret",
