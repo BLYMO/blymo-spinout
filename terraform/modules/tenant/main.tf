2f04d7c018ce76ef7318e1e28ea2002fceb0e8cf
bastiyan <bastiyan@blymo.co..uk>
2026-04-18 02:32:56 +0100

    give cloudwatch permissions

--- terraform/modules/tenant/main.tf
+++ terraform/modules/tenant/main.tf
@@ -221,6 +221,24 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role_policy" "ecs_execution_logs" {
+  name = "n8n-execution-logs-${var.tenant_id}"
+  role = aws_iam_role.ecs_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
 # Role for the n8n container itself, granting it permissions.
 resource "aws_iam_role" "ecs_task_role" {
   name = "n8n-task-role-${var.tenant_id}"
